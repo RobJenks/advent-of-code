f22fde4e24c0e7f55b4d474d9fc395851c1815c2
Rob Jenks <robjenks@gmail.com>
2018-12-12 23:22:01 +0100

    Add day 3, part 2 solution

--- 2018/cpp/day3/day3.cpp
+++ 2018/cpp/day3/day3.cpp
@@ -8,6 +8,7 @@ void Day3::Run(void) const
     std::cout << "\nDay 3:\n";
 
     Part1();
+    Part2();
 }
 
 void Day3::Part1(void) const
@@ -30,7 +31,28 @@ void Day3::Part1(void) const
     int result = 0;
     for (int i = 0; i < AREA_SIZE; ++i) { result += (overlapping[i] ? 1 : 0); }
     
-    std::cout << "Part 1 result = " << result;
+    std::cout << "Part 1 result = " << result << "\n";
+}
+
+void Day3::Part2(void) const
+{
+    std::vector<std::string> input = GetLines(ReadInput(fs::path("day3/input.txt")));
+
+    std::vector<Claim> claims;
+    std::transform(input.begin(), input.end(), std::back_inserter(claims),
+        [this](const std::string & el) { return this->ParseClaim(el); });
+
+    // Record claims and determine overlap
+    std::bitset<AREA_SIZE> occupied = { 0 };
+    std::bitset<AREA_SIZE> overlapping = { 0 };
+    std::for_each(claims.begin(), claims.end(), [this, &occupied, &overlapping](const auto & el) { RecordClaim(el, occupied, overlapping); });
+
+    // Guaranteed exactly one match
+    int result = std::find_if(claims.begin(), claims.end(), [this, &overlapping](const auto & el) 
+        { return !this->ClaimOverlaps(el, overlapping); }
+    )->ID;
+
+    std::cout << "Part 2 result = " << result << "\n";
 }
 
 Day3::Claim Day3::ParseClaim(const std::string & claim_entry) const
@@ -56,4 +78,17 @@ void Day3::RecordClaim(const Claim & claim, std::bitset<AREA_SIZE> & occupancy, 
             occupancy[index] = true;
         }
     }
+}
+
+bool Day3::ClaimOverlaps(const Claim & claim, const std::bitset<AREA_SIZE> & overlap_data) const
+{
+    for (int x = claim.Left; x < (claim.Left + claim.Width); ++x)
+    {
+        for (int y = claim.Top; y < (claim.Top + claim.Height); ++y)
+        {
+            if (overlap_data[Index(x, y)]) return true;
+        }
+    }
+
+    return false;
 }
--- 2018/cpp/day3/day3.h
+++ 2018/cpp/day3/day3.h
@@ -13,6 +13,9 @@ public:
 
 private:
 
+    void Part1(void) const;
+    void Part2(void) const;
+
     class Claim
     {
     public:
@@ -22,15 +25,13 @@ private:
         Claim() : ID(0), Left(0), Top(0), Width(0), Height(0) { }
     };
 
-
     static const int AREA_DIM = 1000;
     static const int AREA_SIZE = (AREA_DIM * AREA_DIM);
 
     // Transform to linear data for efficiency
     static inline int Index(int x, int y) { return x + (y * AREA_DIM); }
 
-    void Part1(void) const;
-
     Claim ParseClaim(const std::string & claim_entry) const;
     void RecordClaim(const Claim & claim, std::bitset<AREA_SIZE> & occupancy, std::bitset<AREA_SIZE> & overlaps) const;
+    bool ClaimOverlaps(const Claim & claim, const std::bitset<AREA_SIZE> & overlap_data) const;
 };
