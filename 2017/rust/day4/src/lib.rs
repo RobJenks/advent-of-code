06c16e502c29a5414d194a948ab04d8fb3498b7e
Rob Jenks <robjenks@gmail.com>
2019-01-22 23:19:12 +0000

    Add day 4, part 1 solution

--- 2017/rust/day4/src/lib.rs
+++ 2017/rust/day4/src/lib.rs
@@ -6,8 +6,8 @@ fn part1() -> u32 {
     common::read_file("day4/input.txt")
         .split("\n")
         .map(|x| x.split_whitespace().collect::<Vec<&str>>())
-        .map(|vec| *&vec[0..vec.len() - 1].iter().enumerate()        // Iter words [0 n-1)   [i.e. skip last]
-            .all(|(i, x)| !(&vec[i + 1..].contains(x)).clone())
+        .map(|vec| *&vec[0..vec.len()-1].iter().enumerate()        // Iter words [0 n-1)   [i.e. skip last]
+            .all(|(i, x)| !(&vec[(i+1)..].contains(x)).clone())
         )
         .filter(|pr| *pr)
         .count() as u32
