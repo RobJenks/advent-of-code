c0bacdf37d23d0148b6e4efc3c7a69b1580378af
Rob Jenks <robjenks@gmail.com>
2019-01-26 19:02:28 +0000

    Add day 9, part 1 solution

---
+++ 2017/rust/day9/src/lib.rs
@@ -0,0 +1,78 @@
+pub fn run() {
+    println!("Part 1 result: {}", part1());
+}
+
+fn part1() -> u32 {
+    Stream::new(common::read_file("day9/input.txt").as_str()).map(|(_, x)| x).sum()
+}
+
+struct Stream {
+    data: Vec<char>,
+    ptr: usize,
+    level: u32,
+}
+
+impl Stream {
+    fn new(input: &str) -> Self { Stream { data: input.chars().collect::<Vec<char>>(), ptr: 0, level: 0 }}
+}
+
+impl Iterator for Stream {
+    type Item = (usize, u32);       // { index, group level }
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut in_garbage = false;
+        loop {
+            let ch = self.data[self.ptr];
+            if ch == '{' && !in_garbage {
+                self.level += 1;
+                self.ptr += 1;
+                return Some((self.ptr - 1, self.level));
+            }
+            else if ch == '}' && !in_garbage {
+                self.level -= 1;
+                if self.level == 0 { return None; }
+            }
+            else if ch == '<' {
+                in_garbage = true;
+            }
+            else if ch == '>' {
+                in_garbage = false;
+            }
+            else if ch == '!' {
+                self.ptr += 1;      // Skip next char
+            }
+
+            self.ptr += 1;
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::{Stream};
+
+    #[test]
+    fn test_count() {
+        assert_eq!(Stream::new("{}").count(), 1);
+        assert_eq!(Stream::new("{{{}}}").count(), 3);
+        assert_eq!(Stream::new("{{},{}}").count(), 3);
+        assert_eq!(Stream::new("{{{},{},{{}}}}").count(), 6);
+        assert_eq!(Stream::new("{<{},{},{{}}>}").count(), 1);
+        assert_eq!(Stream::new("{<a>,<a>,<a>,<a>}").count(), 1);
+        assert_eq!(Stream::new("{{<a>},{<a>},{<a>},{<a>}}").count(), 5);
+        assert_eq!(Stream::new("{{<!>},{<!>},{<!>},{<a>}}").count(), 2);
+    }
+
+    #[test]
+    fn test_scores() {
+        assert_eq!(Stream::new("{}").map(|(_, x)| x).sum::<u32>(), 1);
+        assert_eq!(Stream::new("{{{}}}").map(|(_, x)| x).sum::<u32>(), 6);
+        assert_eq!(Stream::new("{{},{}}").map(|(_, x)| x).sum::<u32>(), 5);
+        assert_eq!(Stream::new("{{{},{},{{}}}}").map(|(_, x)| x).sum::<u32>(), 16);
+        assert_eq!(Stream::new("{<a>,<a>,<a>,<a>}").map(|(_, x)| x).sum::<u32>(), 1);
+        assert_eq!(Stream::new("{{<ab>},{<ab>},{<ab>},{<ab>}}").map(|(_, x)| x).sum::<u32>(), 9);
+        assert_eq!(Stream::new("{{<!!>},{<!!>},{<!!>},{<!!>}}").map(|(_, x)| x).sum::<u32>(), 9);
+        assert_eq!(Stream::new("{{<a!>},{<a!>},{<a!>},{<ab>}}").map(|(_, x)| x).sum::<u32>(), 3);
+    }
+}
