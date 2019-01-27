aa1b10b59f866bf7a42a0e5b1e185803e956045e
Rob Jenks <robjenks@gmail.com>
2019-01-27 00:09:37 +0000

    Add day 10 solutions

--- 2017/rust/src/main.rs
+++ 2017/rust/src/main.rs
@@ -2,7 +2,7 @@
 fn main() {
     let solutions = [
         day1::run, day2::run, day3::run, day4::run, day5::run,
-        day6::run, day7::run, day8::run, day9::run
+        day6::run, day7::run, day8::run, day9::run, day10::run
     ];
 
     for (i, &soln) in solutions.iter().enumerate() {
