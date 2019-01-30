0e80deb70a2be31b39ed6400eb63820328dc9cef
Rob Jenks <robjenks@gmail.com>
2019-01-30 22:42:49 +0000

    Add day 18, part 2 solution

--- 2017/rust/day18/src/lib.rs
+++ 2017/rust/day18/src/lib.rs
@@ -1,14 +1,51 @@
 pub fn run() {
     println!("Part 1 result: {}", part1());
+    println!("Part 2 result: {}", part2());
 }
 
 fn part1() -> isize {
-    CPU::new(100, parse_instructions(common::read_file("day18/input.txt"))).by_ref()
+    CPU::new(100, parse_instructions(common::read_file("day18/input.txt"), APIVersion::V1)).by_ref()
         .take_while(|st| st.rcv_count == 0)
         .last()
         .unwrap()
         .out
 }
+
+
+fn part2() -> usize {
+    run_multiple(
+        &parse_instructions(common::read_file("day18/input.txt"), APIVersion::V2), 100
+    )[1].snd_count
+}
+
+
+fn run_multiple(instr: &Vec<Instruction>, reg_count: usize) -> Vec<CPUState> {
+    let mut cpu = (0..2).map(|x| CPU::new(100, instr.clone())).collect::<Vec<CPU>>();
+
+    cpu.iter_mut().enumerate().for_each(|(i,x)| x.set_val(&RegVal::Reg{x: 'p'}, i as isize));
+
+    loop {
+        let active = (0..2).map(|i| {
+            match &cpu[i].by_ref().next() {
+                Some(st) if st.sending => {
+                    cpu[(i+1)%2].receive_value(st.out);
+                    cpu[i].state.sending = false;
+                    true
+                },
+                Some(st) if st.receiving => false,
+                None => false,
+
+                _ => true     // Any other instruction
+            }
+        })
+        .collect::<Vec<bool>>();
+
+        if active.iter().all(|x| !*x) { break; }
+    }
+
+    vec![cpu[0].state.clone(), cpu[1].state.clone()]
+}
+
 
 pub struct CPU {
     reg : Vec<isize>,           // Registers
@@ -20,21 +57,30 @@ pub struct CPU {
 
 #[derive(Clone, Debug)]
 pub struct CPUState {
-    out: isize,                 // Last output frequency
-    rcv: isize,                 // Last recovered output frequency
-    rcv_count: usize            // The number of receive actions triggered
+    out: isize,                 // Last output message
+    rcv: Vec<isize>,            // Input message queue
+    snd_count: usize,           // The number of send actions triggered
+    rcv_count: usize,           // The number of receive actions triggered
+    sending: bool,              // Indicates that the output pipe has sent a new value (through 'out'); set until cleared by caller
+    receiving: bool,            // Indicates that the input pipe is waiting for a new value (through 'rcv')
 }
 
 #[allow(non_camel_case_types)]
 #[derive(Clone, Debug)]
 pub enum Instruction {
-    snd{x: RegVal},
+    snd{x: RegVal, v: APIVersion},
     set{x: RegVal, y: RegVal},
     add{x: RegVal, y: RegVal},
     mul{x: RegVal, y: RegVal},
     mdo{x: RegVal, y: RegVal},
-    rcv{x: RegVal},
+    rcv{x: RegVal, v: APIVersion},
     jgz{x: RegVal, y: RegVal}
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum APIVersion {
+    V1,
+    V2
 }
 
 #[derive(Clone, Debug)]
@@ -45,7 +91,7 @@ pub enum RegVal {
 
 impl CPU {
     pub fn new(register_count: usize, instr: Vec<Instruction>) -> Self {
-        Self { reg: vec![0isize; register_count], instr, ip: 0, state: CPUState { out: 0, rcv: 0, rcv_count: 0 } }
+        Self { reg: vec![0isize; register_count], instr, ip: 0, state: CPUState { out: 0, rcv: vec![], snd_count: 0, rcv_count: 0, sending: false, receiving: false } }
     }
 
     fn reg(&self, rv: &RegVal) -> isize {
@@ -71,24 +117,44 @@ impl CPU {
         let ri = self.reg_index(reg);
         self.reg[ri as usize] = val;
     }
+
+    fn receive_value(&mut self, val: isize) {
+        self.state.rcv.push(val);
+    }
 }
 
 impl Iterator for CPU {
     type Item = CPUState;
-    fn next(&mut self) -> Option<Self::Item> {
+    fn next(&mut self) -> Option<CPUState> {
         let instr = self.instr[self.ip as usize].clone();
 
         //println!("Executing instruction {:?} with reg: {:?}, ip: {}, out: {}, rcv: {}", instr, self.reg, self.ip, self.state.out, self.state.rcv);
         match &instr {
-            Instruction::snd {x} => self.state.out = self.reg(&x),
             Instruction::set {x,y} => self.set(&x, &y),
             Instruction::add {x,y} => self.set_val(&x, self.reg(&x) + self.reg(&y)),
             Instruction::mul {x,y} => self.set_val(&x, self.reg(&x) * self.reg(&y)),
             Instruction::mdo {x,y} => self.set_val(&x, self.reg(&x) % self.reg(&y)),
-            Instruction::rcv {ref x} if self.reg(&x) != 0 => { self.state.rcv = self.state.out; self.state.rcv_count += 1 },
-            Instruction::rcv {x:_} => (),
             Instruction::jgz {ref x, ref y} if self.reg(&x) > 0 => self.ip += (self.reg(&y) - 1) as isize,  // -1 since we will increment next
-            Instruction::jgz {x:_,y:_} => ()
+            Instruction::jgz {x:_,y:_} => (),
+
+            Instruction::snd {x, v} if *v == APIVersion::V1 => { self.state.out = self.reg(&x); self.state.snd_count += 1; },
+            Instruction::snd {x, v} if *v == APIVersion::V2 => {
+                if self.state.sending { panic!("Sending through output pipe before last value has been consumed; data loss"); }
+                self.state.sending = true;
+                self.state.snd_count += 1;
+                self.state.out = self.reg(&x);
+            },
+
+            Instruction::rcv {ref x, v} if *v == APIVersion::V1 && self.reg(&x) != 0 => { self.state.rcv_count += 1 },
+            Instruction::rcv {x:_, v} if *v == APIVersion::V1 => (),
+            Instruction::rcv {x, v} if *v == APIVersion::V2 => {
+                if self.state.rcv.is_empty() { self.state.receiving = true; return Some(self.state.clone()); }
+                let val = self.state.rcv.remove(0);
+                self.set_val(&x, val);
+                self.state.receiving = false;
+            },
+
+            _ => panic!("Unknown instruction")
         }
 
         self.ip += 1;
@@ -106,17 +172,17 @@ impl RegVal {
 }
 
 
-fn parse_instructions(input: String) -> Vec<Instruction> {
+fn parse_instructions(input: String, api_version: APIVersion) -> Vec<Instruction> {
     input.split("\n")
         .map(|s| s.split_whitespace().collect::<Vec<&str>>())
         .map(|s| match *s.first().unwrap() {
-            "snd" => Instruction::snd{ x: RegVal::new(s[1]) },
+            "snd" => Instruction::snd{ x: RegVal::new(s[1]), v: api_version.clone() },
             "set" => Instruction::set{ x: RegVal::new(s[1]), y: RegVal::new(s[2])},
             "add" => Instruction::add{ x: RegVal::new(s[1]), y: RegVal::new(s[2])},
             "mul" => Instruction::mul{ x: RegVal::new(s[1]), y: RegVal::new(s[2])},
             "mod" => Instruction::mdo{ x: RegVal::new(s[1]), y: RegVal::new(s[2])},
-            "rcv" => Instruction::rcv{ x: RegVal::new(s[1]) },
             "jgz" => Instruction::jgz{ x: RegVal::new(s[1]), y: RegVal::new(s[2])},
+            "rcv" => Instruction::rcv{ x: RegVal::new(s[1]), v: api_version.clone() },
             _ => panic!("Unknown instruction")
         })
         .collect::<Vec<Instruction>>()
@@ -124,16 +190,15 @@ fn parse_instructions(input: String) -> Vec<Instruction> {
 
 #[cfg(test)]
 mod tests {
-    use super::{CPU, Instruction, RegVal, parse_instructions };
+    use super::{CPU, Instruction, RegVal, APIVersion, parse_instructions, run_multiple };
 
     #[test]
     fn test_execution() {
         assert_eq!(
-            CPU::new(5, parse_instructions(common::read_file("tests.txt"))).by_ref()
+            CPU::new(5, parse_instructions(common::read_file("tests.txt"), APIVersion::V1)).by_ref()
                 .take_while(|st| st.rcv_count == 0)
                 .last()
                 .unwrap()
                 .out, 4);
     }
-
 }
