0a7e21d82d3aa17e9bbbef37a2dc1f2095d491dc
Rob Jenks <robjenks@gmail.com>
2019-02-08 23:57:34 +0000

    Add day 3, part 2 solution

--- 2016/python/day3/day3.py
+++ 2016/python/day3/day3.py
@@ -1,11 +1,21 @@
+from operator import concat
+from functools import reduce
 from common import io
 
 def run():
     print("Part 1 result:", part1())
+    print("Part 2 result:", part2())
 
 
 def part1():
     return sum(1 for x in parse_input("day3/input.txt") if sum(sorted(x)[:2]) > sorted(x)[-1])
+
+
+def part2():
+    data = parse_input("day3/input.txt")
+    transposed = reduce(concat, [list(zip(*data[i:i+3])) for i in range(0, len(data), 3)])
+
+    return sum(1 for x in transposed if sum(sorted(x)[:2]) > sorted(x)[-1])
 
 
 def parse_input(path):
