441d9dc2551fb5f9c3d4b15c6e3cddb284819208
Rob Jenks <robjenks@gmail.com>
2019-02-10 18:27:53 +0000

    Add day 7, part 2 solution

--- 2016/python/day7/day7.py
+++ 2016/python/day7/day7.py
@@ -1,15 +1,21 @@
+import re
 import unittest
 from common import io
 
 def run():
     print("Part 1 result:", part1())
+    print("Part 2 result:", part2())
 
 
 def part1():
-    return sum(1 for x in io.read_file("day7/input.txt").splitlines(False) if is_valid(x))
+    return sum(1 for x in io.read_file("day7/input.txt").splitlines(False) if is_tls(x))
 
 
-def is_valid(addr):
+def part2():
+    return sum(1 for x in io.read_file("day7/input.txt").splitlines(False) if is_ssl(x))
+
+
+def is_tls(addr):
     found, bracketed = [], False
     for (i, x) in enumerate(addr[:-3]):
         bracketed = True if x == '[' else False if x == ']' else bracketed
@@ -20,9 +26,26 @@ def is_valid(addr):
     return found
 
 
+def is_ssl(addr):
+    aba, bracketed = [], False
+    for (i, x) in enumerate(addr[:-2]):
+        bracketed = True if x == '[' else False if x == ']' else bracketed
+        if not bracketed and x == addr[i + 2] and x != addr[i + 1]:
+            aba.append(addr[i:i+3])
+
+    return sum(1 for x in aba if re.search(r"\[[a-z]*?{}[a-z]*?\]".format(re.escape(''.join([x[1], x[0], x[1]]))), addr)) != 0
+
+
 class Tests(unittest.TestCase):
     def test_tls(self):
-        self.assertTrue(is_valid("abba[mnop]qrst"))
-        self.assertFalse(is_valid("abcd[bddb]xyyx"))
-        self.assertFalse(is_valid("aaaa[qwer]tyui"))
-        self.assertTrue(is_valid("ioxxoj[asdfgh]zxcvbn"))
+        self.assertTrue(is_tls("abba[mnop]qrst"))
+        self.assertFalse(is_tls("abcd[bddb]xyyx"))
+        self.assertFalse(is_tls("aaaa[qwer]tyui"))
+        self.assertTrue(is_tls("ioxxoj[asdfgh]zxcvbn"))
+
+    def test_ssl(self):
+        self.assertTrue(is_ssl("aba[bab]xyz"))
+        self.assertFalse(is_ssl("xyx[xyx]xyx"))
+        self.assertTrue(is_ssl("aaa[kek]eke"))
+        self.assertTrue(is_ssl("zazbz[bzb]cdb"))
+        self.assertFalse(is_ssl("aba[cd]bab[dc]"))
