d8ff6e9a85d4880694b4986f7187bfb965f0a79d
Rob Jenks <robjenks@gmail.com>
2019-02-10 19:35:14 +0000

    Add list rotation methods to common library

--- 2016/python/common/util.py
+++ 2016/python/common/util.py
@@ -1,2 +1,21 @@
+from math import fmod
+import unittest
+
+
 def clamp(x, l, u):
     return l if (x < l) else u if (x > u) else x
+
+
+def rotate(lst, n):
+    n = int(fmod(n, len(lst)))
+    return lst[n:] + lst[:n]
+
+
+
+class Tests(unittest.TestCase):
+    def test_rotate(self):
+        self.assertEqual([1, 2, 3, 4, 5], rotate([1, 2, 3, 4, 5], 0))
+        self.assertEqual([4, 5, 1, 2, 3], rotate([1, 2, 3, 4, 5], 3))
+        self.assertEqual([4, 5, 1, 2, 3], rotate([1, 2, 3, 4, 5], 3 + 5 + 5 + 5))
+        self.assertEqual([2, 3, 4, 5, 1], rotate([1, 2, 3, 4, 5], -4))
+        self.assertEqual([2, 3, 4, 5, 1], rotate([1, 2, 3, 4, 5], -14))
