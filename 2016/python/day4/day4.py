07b2acf1fd385a9685c790786b710656ca592206
Rob Jenks <robjenks@gmail.com>
2019-02-10 12:59:48 +0000

    Add day 4, part 2 solution

--- 2016/python/day4/day4.py
+++ 2016/python/day4/day4.py
@@ -4,10 +4,16 @@ from common import io
 
 def run():
     print("Part 1 result:", part1())
+    print("Part 2 result:", part2())
 
 
 def part1():
     return sum(int(x[1]) for x in parse_input("day4/input.txt") if checksum(x[0]) == x[2])
+
+
+def part2():
+    real = list((x[0], int(x[1])) for x in parse_input("day4/input.txt") if checksum(x[0]) == x[2])
+    return [x[1] for x in real if 'northpole' in decrypt(*x)]
 
 
 def parse_input(path):
@@ -26,3 +32,11 @@ def checksum(name):
     c = list(Counter([x for x in name if x != '-']).items())
     c = sorted(c, key=lambda x: x[0], reverse=False)
     return ''.join([x[0] for x in sorted(c, key=lambda x: x[1], reverse=True)[:5]])
+
+
+def decrypt(name, id):
+    return ''.join([cycle_char(x, id) for x in name])
+
+
+def cycle_char(ch, id):
+    return chr((((ord(ch) - ord('a')) + id) % 26) + ord('a')) if ch != '-' else ' '
