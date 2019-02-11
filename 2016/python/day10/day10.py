18e9a37d753ab218b90cac92e0e773f21ba561f8
Rob Jenks <robjenks@gmail.com>
2019-02-11 00:06:33 +0000

    Add day 10, part 1 solution

---
+++ 2016/python/day10/day10.py
@@ -0,0 +1,61 @@
+from enum import IntEnum
+from common import io
+
+def run():
+    print("Part 1 result:", part1())
+
+
+def part1():
+    return [x for x in follow_instructions(parse_input("day10/input.txt"))
+        if x[1][0][0] == 17 and x[1][1][0] == 61][0][0]
+
+
+def follow_instructions(instr):
+    # Perform all one-time initialisation before the generator cycle
+    bot_count = list(max([[x[i] for i in [2, 4] if x[i+1] == DestType.Bot] for x in instr if x[0] == InstructionType.Give]))[0] + 1
+    out_count = list(max([[x[i] for i in [2, 4] if x[i+1] == DestType.Output] for x in instr if x[0] == InstructionType.Give]))[0] + 1
+
+    bots = [[] for _ in range(bot_count)]
+    outputs = [[] for _ in range(out_count)]
+    dist = {x[1]: ((x[2], x[3]), (x[4], x[5])) for x in instr if x[0] == InstructionType.Give}
+
+    for x in instr:
+        if x[0] == InstructionType.Init:
+            bots[x[1]].append(x[2])
+
+    # Evaluation cycle
+    eval = [i for (i, x) in enumerate(bots) if len(x) == 2]
+    while eval:
+        i = eval.pop()
+        vals = sorted(bots[i])
+        assign = dist[i]
+
+        yield((i, [(vals[ix], assign[ix]) for ix in range(2)]))  # Yield { bot, [(val0, tgt0), (val1, tgt1)] }
+
+        bots[i] = []
+        for (i, x) in enumerate(assign):
+            [bots, outputs][x[1] == DestType.Output][x[0]].append(vals[i])
+
+        eval.extend([x[0] for x in assign if x[1] == DestType.Bot and len(bots[x[0]]) == 2])
+
+
+def parse_input(path):
+    rows = [x.split() for x in io.read_file(path).splitlines(False)]
+    return [
+        (InstructionType.Init, int(x[5]), int(x[1])) if x[2] == 'goes'      # { bot, init-value }
+        else (InstructionType.Give, int(x[1]),                              # { bot, give-low, type-low, give-high, type-high }
+              int(x[6]), DestType.Output if x[5] == 'output' else DestType.Bot,
+              int(x[11]), DestType.Output if x[10] == 'output' else DestType.Bot) if x[2] == 'gives'
+
+        else "ERROR"
+    for x in rows]
+
+
+class InstructionType(IntEnum):
+    Init = 0
+    Give = 1
+
+class DestType(IntEnum):
+    Bot = 0
+    Output = 1
+
--- 2016/python/main.py
+++ 2016/python/main.py
@@ -7,12 +7,13 @@ from day6 import day6
 from day7 import day7
 from day8 import day8
 from day9 import day9
+from day10 import day10
 
 
 def main():
     solutions = [
         day1.run, day2.run, day3.run, day4.run, day5.run,
-        day6.run, day7.run, day8.run, day9.run
+        day6.run, day7.run, day8.run, day9.run, day10.run
     ]
 
     for (i, soln) in enumerate(solutions):
