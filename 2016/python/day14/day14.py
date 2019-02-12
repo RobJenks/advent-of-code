c8af99591fa2f0998fb0416822873806c7bc28f4
Rob Jenks <robjenks@gmail.com>
2019-02-12 23:07:59 +0000

    Add day 14, part 2 solution

--- 2016/python/day14/day14.py
+++ 2016/python/day14/day14.py
@@ -8,18 +8,23 @@ def input():
 
 def run():
     print("Part 1 result:", part1(input()))
+    print("Part 2 result:", part2(input()))
 
 
 def part1(input):
-    return get_keys(input, 64)[-1]
+    return get_keys(input, 64, get_hash)[-1]
 
 
-def get_keys(input, n) -> List[int]:    # [ indices ]
+def part2(input):
+    return get_keys(input, 64, get_stretched_hash)[-1]
+
+
+def get_keys(input, n, generator) -> List[int]:    # [ indices ]
     index, res = 0, []
     pending: List[Tuple[bool, int, str]] = []     # [{ active, index, char[5] }]
 
     for index in count():
-        hash = get_hash(input, index)
+        hash = generator(input, index)
 
         # Validate any pending hashes, if possible
         threshold = index - 1000
@@ -42,11 +47,26 @@ def get_hash(salt, index):
     return hashlib.md5(format("{}{}".format(salt, index)).encode()).hexdigest()
 
 
+def get_stretched_hash(salt, index):
+    x = format("{}{}".format(salt, index))
+    for i in range(2017):
+        x = hashlib.md5(x.encode()).hexdigest()
+
+    return x
+
+
 class Tests(TestCase):
     def test_key_generation(self):
-        keys = get_keys("abc", 64)
-        print(keys)
+        keys = get_keys("abc", 64, get_hash)
 
         self.assertEqual(39, keys[0])
         self.assertEqual(92, keys[1])
         self.assertEqual(22728, keys[63])
+
+    def test_stretched_hash(self):
+        self.assertEqual("a107ff634856bb300138cac6568c0f24", get_stretched_hash("abc", 0))
+
+    def test_stretched_key_generation(self):
+        keys = get_keys("abc", 64, get_stretched_hash)
+        self.assertEqual(10, keys[0])
+        self.assertEqual(22551, keys[63])
