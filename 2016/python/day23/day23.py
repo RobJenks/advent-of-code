1f4af033157e628628da7230da6bec508211d8f1
Rob Jenks <robjenks@gmail.com>
2019-02-16 23:24:52 +0000

    Add day 23, part 2 solution

--- 2016/python/day23/day23.py
+++ 2016/python/day23/day23.py
@@ -1,13 +1,22 @@
 from unittest import TestCase
+from functools import reduce
+from operator import mul
+from math import factorial
 from day12 import day12
 
 def run():
     print("Part 1 result:", part1())
+    print("Part 2 result:", part2())
 
 
 def part1():
     (reg_final, _) = day12.execute(day12.parse_input("day23/input.txt"), [7, 0, 0, 0])
     return reg_final[0]
+
+
+def part2():
+    inst = day12.parse_input("day23/input.txt")
+    return reduce(mul, [inst[i].args[0] for i in range(19, 21)], 1) + factorial(12)
 
 
 class Tests(TestCase):
