6a68b8512a13fa7bfef459fcf70edc889405c823
Rob Jenks <robjenks@gmail.com>
2019-02-16 12:58:39 +0000

    Enhance day12 interpreter to support day23 execution

--- 2016/python/day12/day12.py
+++ 2016/python/day12/day12.py
@@ -4,20 +4,18 @@ from enum import IntEnum
 from common import io
 
 def run():
-    print("Part 1 result:", part1())
-    print("Part 2 result:", part2())
+    print("Part 1 result:", part1("day12/input.txt"))
+    print("Part 2 result:", part2("day12/input.txt"))
 
 
-def part1():
-    inst = parse_input("day12/input.txt")
-    (reg_final, _) = execute(inst, [0, 0, 0, 0])
+def part1(input):
+    (reg_final, _) = execute(parse_input(input), [0, 0, 0, 0])
 
     return reg_final[0]
 
 
-def part2():
-    inst = parse_input("day12/input.txt")
-    (reg_final, _) = execute(inst, [0, 0, 1, 0])
+def part2(input):
+    (reg_final, _) = execute(parse_input(input), [0, 0, 1, 0])
 
     return reg_final[0]
 
@@ -28,22 +26,29 @@ def parse_input(path):
 
 
 def execute(instructions, registers: List[int]) -> (List[int], int):    # Returns (Registers, PC)
-    pc = 0
-    while pc < len(instructions):
+    pc, n = 0, len(instructions)
+    while pc < n:
         inst = instructions[pc]
         if inst.typ == Inst.cpy:
-            registers[register_id(inst.args[1])] = \
-                registers[register_id(inst.args[0])] if isinstance(inst.args[0], str) else inst.args[0]
+            if is_numeric(inst.args[1]): continue
+            registers[register_id(inst.args[1])] = resolve_arg(registers, inst.args[0])
 
         elif inst.typ == Inst.inc:
+            if is_numeric(inst.args[0]): continue
             registers[register_id(inst.args[0])] += 1
 
         elif inst.typ == Inst.dec:
+            if is_numeric(inst.args[0]): continue
             registers[register_id(inst.args[0])] -= 1
 
         elif inst.typ == Inst.jnz:
-            if 0 != (registers[register_id(inst.args[0])] if isinstance(inst.args[0], str) else inst.args[0]):
-                pc += (inst.args[1] - 1)    # -1 since we will increment the PC immediately after each instruction
+            if resolve_arg(registers, inst.args[0]) != 0:
+                pc += (resolve_arg(registers, inst.args[1]) - 1)    # -1 since we will increment the PC
+                                                                    # immediately after each instruction
+        elif inst.typ == Inst.tgl:
+            tgt = pc + resolve_arg(registers, inst.args[0])
+            if 0 <= tgt < n:
+                instructions[tgt] = toggle(instructions[tgt])
 
         pc += 1
 
@@ -54,8 +59,21 @@ def register_id(reg: str) -> int:
     return ord(reg) - ord('a')
 
 
+def resolve_arg(reg, arg):
+    return reg[register_id(arg)] if isinstance(arg, str) else arg
+
+
 def is_numeric(x) -> bool:
     return x.isnumeric() or (x[1:].isnumeric() and x[0:1] == '-')
+
+
+def toggle(inst):
+    if len(inst.args) == 1:
+        return Instruction(Inst.dec if inst.typ == Inst.inc else Inst.inc, inst.args)
+    elif len(inst.args) == 2:
+        return Instruction(Inst.cpy if inst.typ == Inst.jnz else Inst.jnz, inst.args)
+    else:
+        raise RuntimeError("Cannot toggle unknown instruction type")
 
 
 class Inst(IntEnum):
@@ -63,6 +81,7 @@ class Inst(IntEnum):
     inc = 1
     dec = 2
     jnz = 3
+    tgl = 4     # Day 23 extension
 
 
 class Instruction:
@@ -80,12 +99,16 @@ class Instruction:
         return self.__str__()
 
 
-INST_NAMES = {Inst.cpy: "cpy", Inst.inc: "inc", Inst.dec: "dec", Inst.jnz: "jnz"}
+INST_NAMES = {Inst.cpy: "cpy", Inst.inc: "inc", Inst.dec: "dec", Inst.jnz: "jnz", Inst.tgl: "tgl"}
 INST_ID = dict([(y, x) for (x, y) in INST_NAMES.items()])
 
 
 class Tests(TestCase):
     def test_program(self):
         inst = parse_input("tests.txt")
-        (reg_final, _) = list(execute(inst, [0, 0, 0, 0]))[-1]
+        (reg_final, _) = execute(inst, [0, 0, 0, 0])
         self.assertEqual(42, reg_final[0])
+
+    def test_regression(self):
+        self.assertEqual(318007, part1("input.txt"))
+        self.assertEqual(9227661, part2("input.txt"))
