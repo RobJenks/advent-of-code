749070cac65159865fbd1da3caf5c5330a7e04b3
Rob Jenks <robjenks@gmail.com>
2019-05-11 17:41:34 +0000

    Add common io package

---
+++ 2015/go/common/input.go
@@ -0,0 +1,37 @@
+package input
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// ReadFile : Read an entire file and return the contents as text
+func ReadFile(path string) string {
+	f, err := os.Open(path)
+	if err != nil {
+		panic(fmt.Sprintf("Error opening file \"%s\": %s", path, err.Error()))
+	}
+
+	defer f.Close()
+
+	str := ""
+	buffer := make([]byte, 128)
+	for {
+		n, err := f.Read(buffer)
+		if n == 0 {
+			return str
+		}
+
+		if err != nil {
+			panic(fmt.Sprintf("Error reading data from \"%s\": %s", path, err.Error()))
+		}
+
+		str += string(buffer[0:n])
+	}
+}
+
+// GetLines : Split a text block around newline characters
+func GetLines(data string) []string {
+	return strings.Split(data, "\n")
+}
