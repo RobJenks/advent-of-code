dc1cd691a4f3022949db3292945b5b64464b2559
Rob Jenks <robjenks@gmail.com>
2019-05-15 17:28:20 +0000

    Add day 3, part 2 solution

--- 2015/go/day3/day3.go
+++ 2015/go/day3/day3.go
@@ -9,10 +9,22 @@ import (
 // Day3 : Solutions
 func Day3() {
 	fmt.Println("Part 1 result:", part1())
+	fmt.Println("Part 2 result:", part2())
 }
 
 func part1() int {
 	visits := getVisits(common.ReadFile("day3/input.txt"))
+	return len(visits)
+}
+
+func part2() int {
+	input := common.ReadFile("day3/input.txt")
+	routes := [2]string{"", ""}
+	for i, ch := range input {
+		routes[i%2] += string(ch)
+	}
+
+	visits := mapUnion([]map[[2]int]int{getVisits(routes[0]), getVisits(routes[1])})
 	return len(visits)
 }
 
@@ -30,6 +42,17 @@ func getVisits(input string) map[[2]int]int {
 
 func addCoords(a, b [2]int) [2]int {
 	return [2]int{a[0] + b[0], a[1] + b[1]}
+}
+
+func mapUnion(maps []map[[2]int]int) map[[2]int]int {
+	union := make(map[[2]int]int)
+	for _, m := range maps {
+		for k, v := range m {
+			union[k] += v
+		}
+	}
+
+	return union
 }
 
 var delta = map[rune][2]int{
