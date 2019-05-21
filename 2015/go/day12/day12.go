d82acfa9616eea4299f1676cd1e32eeb373a242a
Rob Jenks <robjenks@gmail.com>
2019-05-21 22:21:24 +0000

    Add day 12, part 2 solution

--- 2015/go/day12/day12.go
+++ 2015/go/day12/day12.go
@@ -12,41 +12,53 @@ type jsonObject = map[string]interface{}
 // Day12 : Solutions
 func Day12() {
 	fmt.Println("Part 1 result:", part1())
+	fmt.Println("Part 2 result:", part2())
 }
 
 func part1() int {
 	input := common.ReadFile("day12/input.txt")
-	return accumulate(readJSON(input))
+	return accumulate(readJSON(input), func(_ string) bool { return false })
 }
 
-func accumulate(jsonData interface{}) int {
+func part2() int {
+	input := common.ReadFile("day12/input.txt")
+	return accumulate(readJSON(input), func(x string) bool {
+		return x == "red"
+	})
+}
+
+func accumulate(jsonData interface{}, breakString func(string) bool) int {
 	switch jsonData := jsonData.(type) {
 	case jsonObject:
-		return int(accumulateOverJSON(jsonData))
+		return int(accumulateOverJSON(jsonData, breakString))
 	case []interface{}:
-		return int(accumulateOverJSONArray(jsonData))
+		return int(accumulateOverJSONArray(jsonData, breakString))
 	}
 
 	panic("Unknown deserialized JSON type")
 }
 
-func accumulateOverJSON(data jsonObject) float64 {
+func accumulateOverJSON(data jsonObject, breakString func(string) bool) float64 {
 	var acc float64
 	for _, v := range data {
 		switch v := v.(type) {
+		case string:
+			if breakString(v) {
+				return 0
+			}
 		case float64:
 			acc += v
 		case jsonObject:
-			acc += accumulateOverJSON(v)
+			acc += accumulateOverJSON(v, breakString)
 		case []interface{}:
-			acc += accumulateOverJSONArray(v)
+			acc += accumulateOverJSONArray(v, breakString)
 		}
 	}
 
 	return acc
 }
 
-func accumulateOverJSONArray(data []interface{}) float64 {
+func accumulateOverJSONArray(data []interface{}, breakString func(string) bool) float64 {
 	var acc float64
 
 	for _, el := range data {
@@ -54,9 +66,9 @@ func accumulateOverJSONArray(data []interface{}) float64 {
 		case float64:
 			acc += el
 		case jsonObject:
-			acc += accumulateOverJSON(el)
+			acc += accumulateOverJSON(el, breakString)
 		case []interface{}:
-			acc += accumulateOverJSONArray(el)
+			acc += accumulateOverJSONArray(el, breakString)
 		}
 	}
 
