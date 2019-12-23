6e65db1acd87a9a8ee36e8da81c44e1681ca8974
Rob Jenks <robjenks@gmail.com>
2019-12-23 23:23:46 +0000

    Add unit testing; add day 1, part 2 solution

--- 2019/haskell/Day1/Day1.hs
+++ 2019/haskell/Day1/Day1.hs
@@ -1,21 +1,42 @@
 module Day1.Day1
 ( part1
 , part2
+, tests
 )
 where
 
 import Data.List
+import Control.Exception
 
 part1 :: String -> String
-part1 x = show $ sum $ (map calcFuel (map read (lines x)))
+part1 x = show $ applyToModules fuelReq x
 
 part2 :: String -> String
-part2 x = head $ lines $ x
+part2 x = show $ applyToModules reqAccountingForFuel x 
 
 
-calcFuel :: Int -> Int
-calcFuel x = (x `div` 3) - 2
+applyToModules :: (Int -> Int) -> String -> Int
+applyToModules f x = sum $ (map f (map read (lines x)))
+
+fuelReq :: Int -> Int
+fuelReq x = (x `div` 3) - 2
+
+reqAccountingForFuel :: Int -> Int
+reqAccountingForFuel m
+  | m <= 0    = 0
+  | otherwise = fuel + reqAccountingForFuel fuel
+    where fuel = max 0 (fuelReq m)
 
 
 
+-- Tests
+tests = [test1, test2, test3]
 
+test1 _ = fullReqTest 14 2
+test2 _ = fullReqTest 1969 966
+test3 _ = fullReqTest 100756 50346
+
+
+fullReqTest x exp = assertEqual (reqAccountingForFuel x) exp
+
+assertEqual x exp = if (x == exp) then () else error ("Error: " ++ show x ++ " != " ++ show exp)
--- 2019/haskell/main.hs
+++ 2019/haskell/main.hs
@@ -1,15 +1,20 @@
 import qualified Day1.Day1 as Day1
 
+-- Unit test state for all modules
+skipTests = False
+
+-- Solutions
 data Solution = Solution { day :: Int
                          , part1 :: (String -> String)
                          , part2 :: (String -> String)
+                         , tests :: [(String -> ())]
                          }
 
 main :: IO()
 main = do
-  let solutions = [ Solution { day=1, part1=Day1.part1, part2=Day1.part2 }
-                  , Solution { day=1, part1=Day1.part1, part2=Day1.part2 } ]
-
+  let solutions = [ Solution { day=1, part1=Day1.part1, part2=Day1.part2, tests=Day1.tests }
+                  , Solution { day=1, part1=Day1.part1, part2=Day1.part2, tests=Day1.tests } ]
+  -- putStrLn $ show $ (map id (Day1.tests False))  
   runSolutions solutions 
 
   
@@ -24,6 +29,12 @@ runSolutions (x:xs) = do
 runSolution :: Solution -> IO()
 runSolution solution = do
   input <- readFile ("Day" ++ (show $ day $ solution) ++ "/input")
+  putStrLn ("Day " ++ (show $ day $ solution) ++ ", Tests : " ++ (runTests solution input))
   putStrLn ("Day " ++ (show $ day $ solution) ++ ", Part 1: " ++ (part1 solution $ input))
-  putStrLn ("Day " ++ (show $ day $ solution) ++ ", Part 2: " ++ (part2 solution $ input))
+  putStrLn ("Day " ++ (show $ day $ solution) ++ ", Part 2: " ++ (part2 solution $ input) ++ "\n")
+
+runTests :: Solution -> String -> String
+runTests sln input
+  | skipTests = "Tests skipped"
+  | otherwise = (show $ sum $ (map (\x -> if x == () then 1 else 0) (map ($ input) (tests sln)))) ++ " passed"
 
