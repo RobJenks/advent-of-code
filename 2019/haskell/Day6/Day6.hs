41443abcfa7c5d3182de281f65e53bd31b908061
Rob Jenks <robjenks@gmail.com>
2020-01-02 18:18:56 +0000

    Add day 6, part 2 solution

--- 2019/haskell/Day6/Day6.hs
+++ 2019/haskell/Day6/Day6.hs
@@ -5,6 +5,7 @@ module Day6.Day6
 )
 where
 
+import Data.Maybe
 import Data.Map.Strict (Map(..), (!))
 import qualified Data.Map.Strict as Map
 
@@ -15,12 +16,11 @@ data Obj = Obj { objName     :: String
                , objChildren :: [Obj]  } deriving Show
 
 
-
 part1 :: String -> String
 part1 x = show $ orbitCount $ buildTree (parseInput x) "COM"
 
 part2 :: String -> String
-part2 x = "Not completed"
+part2 x = show $ transferDistance (buildTree (parseInput x) "COM") "YOU" "SAN"
 
 
 buildTree :: Map String [String] -> String -> Obj
@@ -47,15 +47,31 @@ orbitCount obj = aggregateDepth 0 obj
 aggregateDepth :: Int -> Obj -> Int
 aggregateDepth depth obj = depth + (sum $ map (aggregateDepth (depth+1)) (objChildren obj))
 
+pathToNode :: Obj -> String -> Maybe [String]
+pathToNode obj node
+  | ((objName obj) == node) = Just [node]
+  | (null validPaths) = Nothing
+  | ((length validPaths) > 1) = error "Multiple intersecting paths"
+  | otherwise = Just ([objName obj] ++ (fromJust $ head validPaths))
+  where
+    validPaths = ((filter isJust) . (map (\c -> pathToNode c node))) $ objChildren obj
 
+transferDistance :: Obj -> String -> String -> Int
+transferDistance sys src dst = ((length srcPath) - 1) + ((length dstPath) - 1) - (2 * common)
+  where
+    srcPath = fromJust $ pathToNode sys "YOU"
+    dstPath = fromJust $ pathToNode sys "SAN"
+    common = length $ takeWhile (\(x,y) -> x==y) $ zip srcPath dstPath
 
 
 
 -- Tests 
-tests = [testSample]
+tests = [ testOrbitCount, testTransferDistance ]
 
-sampleMap = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L" :: String
+sampleMap1 = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L" :: String
+sampleMap2 = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN" :: String
 
-testSample x = assertEqual (orbitCount $ buildTree (parseInput sampleMap) "COM") 42
+testOrbitCount _ = assertEqual (orbitCount $ buildTree (parseInput sampleMap1) "COM") 42
 
+testTransferDistance _ = assertEqual (transferDistance (buildTree (parseInput sampleMap2) "COM") "YOU" "SAN") 4
 
