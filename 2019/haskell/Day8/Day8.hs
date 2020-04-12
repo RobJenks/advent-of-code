f039cbb4a17be6004d10fdfa7258a75b89cee709
Rob Jenks <robjenks@gmail.com>
2020-04-13 00:38:05 +0100

    Add day 8, part 2 solution

--- 2019/haskell/Day8/Day8.hs
+++ 2019/haskell/Day8/Day8.hs
@@ -9,24 +9,40 @@ import Data.List
 import Data.Char
 import Common.Util
 
+width       = 25 :: Int
+height      =  6 :: Int
+black       =  0 :: Int
+white       =  1 :: Int
+transparent =  2 :: Int
+
 part1 :: String -> String
 part1 x = show $ ((countEl 1 best) * (countEl 2 best))
   where
-    layers = layerData (parseInput x) 25 6
+    layers = layerData (parseInput x) (width * height)
     zeros  = map (\x -> (countEl 0 x, x)) layers
     best   = snd $ foldl (\acc x -> if (fst x) < (fst acc) then x else acc) (999, []) zeros
 
 part2 :: String -> String
-part2 x = "B" 
+part2 x = "\n" ++ (unlines $ rendered)
+  where
+    decoded = decode $ layerData (parseInput x) (width * height)
+    lines = layerData decoded width
+    rendered = map (\line -> (map (\pixel -> if pixel == white then '#' else ' ') line)) lines
 
 
-layerData :: [Int] -> Int -> Int -> [[Int]]
-layerData = buildLinearLayers []
+layerData :: [Int] -> Int -> [[Int]]
+layerData [] _ = [] 
+layerData xs n = [take n xs] ++ layerData (drop n xs) n
 
-buildLinearLayers :: [[Int]] -> [Int] -> Int -> Int -> [[Int]]
-buildLinearLayers acc [] _ _ = acc
-buildLinearLayers acc xs w h = buildLinearLayers (acc ++ [(take n xs)]) (drop n xs) w h
-  where n = w * h
+decode :: [[Int]] -> [Int]
+decode layers
+  | null $ filter (not . null) layers = []
+  | otherwise = [pixel] ++ (decode remainder)
+      where
+        col = map head layers
+        remainder = map tail layers
+        pixel = maybe transparent id (find (/=transparent) col)
+
 
 countFn :: Eq a => (a -> Bool) -> [a] -> Int
 countFn f list = length $ filter f list
@@ -39,7 +55,5 @@ parseInput = map digitToInt . filter (/= '\n')
 
 
 -- Tests 
-tests = [ test1 ]
-
-test1 _ = assertEqual 1 1
+tests = []
 
