bee0b042c7a5acfdcd9e250cff889c53f233cc18
Rob Jenks <robjenks@gmail.com>
2020-12-15 21:34:20 +0000

    Initial 2020 setup; day 1 part 1 solution

---
+++ 2020/rust/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let solutions = [
+        day1::run
+    ];
+
+    solutions.iter().enumerate()
+        .for_each(|(i, solution)| {
+            println!("\nDay {}:\n", i + 1);
+            solution();
+        });
+}
