6360b684b6abf564f2eb13344294065aed9333d9
Rob Jenks <robjenks@gmail.com>
2020-12-16 21:56:50 +0000

    Add day 4, part 2 solution

--- 2020/rust/src/day4/mod.rs
+++ 2020/rust/src/day4/mod.rs
@@ -4,18 +4,25 @@ use super::common;
 
 pub fn run() {
     println!("Part 1 result: {}", part1());
+    println!("Part 2 result: {}", part2());
 }
 
 fn part1() -> usize {
-    get_valid_passport_count(common::read_file("src/day4/problem-input.txt"))
+    get_valid_passport_count(common::read_file("src/day4/problem-input.txt"),
+                             &Passport::has_fields)
 }
 
-fn get_valid_passport_count(input: String) -> usize {
+fn part2() -> usize {
+    get_valid_passport_count(common::read_file("src/day4/problem-input.txt"),
+                             &Passport::is_valid)
+}
+
+fn get_valid_passport_count(input: String, validation_fn: &dyn Fn(&Passport, &HashSet<String>) -> bool) -> usize {
     let mandatory_fields = mandatory_fields();
 
     parse_input(input).iter()
         .map(|x| Passport {fields: x.to_owned()})
-        .filter(|x| x.has_fields(&mandatory_fields))
+        .filter(|x| validation_fn(x, &mandatory_fields))
         .count()
 }
 
@@ -48,6 +55,11 @@ struct Passport {
     fields: Vec<KeyValue>
 }
 
+impl KeyValue {
+    pub fn get_key(&self) -> &String { &self.key }
+    pub fn get_value(&self) -> &String { &self.value }
+}
+
 impl From<String> for KeyValue {
     fn from(x: String) -> Self {
         if let Some((k, v)) = x.split(":").collect_tuple() {
@@ -66,6 +78,56 @@ impl Passport {
             self.fields.iter()
                 .map(|kv| kv.key.clone())
                 .collect::<HashSet<_>>().is_superset(fields)
+    }
+
+    pub fn is_valid(&self, mandatory_fields: &HashSet<String>) -> bool {
+        self.has_fields(mandatory_fields) &&
+            self.fields.iter().all(|x| Self::is_valid_field(x))
+    }
+
+    fn is_valid_field(field: &KeyValue) -> bool {
+        match (field.get_key().as_str(), field.get_value().as_str()) {
+            ("byr", x) => Self::is_valid_year(x, 1920, 2002),
+            ("iyr", x) => Self::is_valid_year(x, 2010, 2020),
+            ("eyr", x) => Self::is_valid_year(x, 2020, 2030),
+            ("hgt", x) => Self::is_valid_height(x),
+            ("hcl", x) => Self::is_valid_hcol(x),
+            ("ecl", x) => Self::is_valid_ecol(x),
+            ("pid", x) => Self::is_valid_pid(x),
+            ("cid", _) => true,
+            _ => false
+        }
+    }
+
+    fn is_valid_year(s: &str, min: u32, max: u32) -> bool {
+        s.len() == 4 &&
+            s.parse::<u32>()
+                .map(|v| v >= min && v <= max)
+                .unwrap_or(false)
+    }
+
+    fn is_valid_height(s: &str) -> bool {
+        s.len() > 2 &&
+            s[0..s.len()-2].parse::<u32>()
+                .map(|v| match &s[s.len()-2..s.len()] {
+                    "cm" => v >= 150 && v <= 193,
+                    "in" => v >= 59 && v <= 76,
+                    _ => false
+                })
+                .unwrap_or(false)
+    }
+
+    fn is_valid_hcol(s: &str) -> bool {
+        s.len() == 7 &&
+            s.chars().skip(1).all(|c| (c >= 'a' && c <= 'f') || (c >= '0' && c <= '9'))
+    }
+
+    fn is_valid_ecol(s: &str) -> bool {
+        ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&s)
+    }
+
+    fn is_valid_pid(s: &str) -> bool {
+        s.len() == 9 && s.parse::<u32>().is_ok()
     }
 }
 
