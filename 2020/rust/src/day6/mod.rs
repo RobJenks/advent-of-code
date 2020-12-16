fee86a8093983613dc38feca20ecb95d910bcde6
Rob Jenks <robjenks@gmail.com>
2020-12-16 23:36:41 +0000

    Add day 6, part 2 solution

--- 2020/rust/src/day6/mod.rs
+++ 2020/rust/src/day6/mod.rs
@@ -3,6 +3,7 @@ use itertools::Itertools;
 
 pub fn run() {
     println!("Part 1 result: {}", part1());
+    println!("Part 2 result: {}", part2());
 }
 
 fn part1() -> usize {
@@ -13,6 +14,13 @@ fn part1() -> usize {
         .sum()
 }
 
+fn part2() -> usize {
+    parse_input(common::read_file("src/day6/problem-input.txt"))
+        .iter()
+        .map(|v| get_unanimous(v).len())
+        .sum()
+}
+
 fn parse_input(input: String) -> Vec<Vec<String>> {
     input.lines().collect::<Vec<_>>()
         .split(|&s| s.trim().is_empty())
@@ -20,14 +28,24 @@ fn parse_input(input: String) -> Vec<Vec<String>> {
         .collect::<Vec<_>>()
 }
 
+fn get_unanimous(values: &Vec<String>) -> Vec<char> {
+    ('a'..='z')
+        .filter(|c| values.iter().all(|s| s.contains(&c.to_string())))
+        .collect()
+}
+
 
 #[cfg(test)]
 mod tests {
-    use super::{ part1 };
+    use super::{ part1, part2 };
 
     #[test]
     fn test_part1() {
         assert_eq!(6549, part1());
     }
 
+    #[test]
+    fn test_part2() {
+        assert_eq!(3466, part2());
+    }
 }
