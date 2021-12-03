fafde4fbff18aef662473c5001bed4f24ac35977
Rob Jenks <robjenks@gmail.com>
2021-12-03 23:28:10 +0000

    Add Rust 2021 and day 1 solutions

---
+++ 2021/rust/src/day1/mod.rs
@@ -0,0 +1,65 @@
+use itertools::Itertools;
+use super::common;
+
+pub fn run() {
+    println!("Part 1 result: {}", part1());
+    println!("Part 2 result: {}", part2());
+}
+
+fn part1() -> usize {
+    count_inc(&parse_input(
+        common::read_file("src/day1/problem-input.txt").as_str()))
+}
+
+fn part2() -> usize {
+    sliding_window(&parse_input(
+        common::read_file("src/day1/problem-input.txt").as_str()))
+}
+
+fn count_inc(values: &Vec<u32>) -> usize {
+    values.windows(2)
+        .map(|w| w.iter().collect_tuple::<(&u32, &u32)>().unwrap_or_else(|| panic!("Invalid input")))
+        .filter(|(&x, &y)| y > x)
+        .count()
+}
+
+fn sliding_window(values: &Vec<u32>) -> usize {
+    values.windows(3)
+        .map(|w| w.iter().sum())
+        .fold((0, None), |(acc, last), el: u32| match last {
+            None => (acc, Some(el)),
+            Some(prev) if el > prev => (acc + 1, Some(el)),
+            Some(_) => (acc, Some(el))
+        }).0
+}
+
+fn parse_input(str: &str) -> Vec<u32> {
+    str.lines()
+        .map(|s| s.parse::<u32>().unwrap_or_else(|_| panic!("Invalid input")))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day1::{ part1, part2, parse_input, count_inc, sliding_window };
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(), 1791);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(), 1822);
+    }
+
+    #[test]
+    fn test_count_inc() {
+        assert_eq!(count_inc(&parse_input("199\n200\n208\n210\n200\n207\n240\n269\n260\n263")), 7);
+    }
+
+    #[test]
+    fn test_sliding_window() {
+        assert_eq!(sliding_window(&parse_input("199\n200\n208\n210\n200\n207\n240\n269\n260\n263")), 5);
+    }
+}
---
+++ 2021/rust/src/main.rs
@@ -0,0 +1,15 @@
+mod common;
+mod day1;
+
+fn main() {
+    let solutions = [
+        day1::run
+    ];
+
+    solutions.iter().enumerate()
+        .for_each(|(i, solution)| {
+            println!("\nDay {}:\n", i + 1);
+            solution();
+        });
+}
+
