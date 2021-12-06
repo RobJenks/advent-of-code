5c3672acb0853019d075577b14e0ce302600a6ef
Rob Jenks <robjenks@gmail.com>
2021-12-06 18:18:01 +0000

    Add day 3, part 2 solution

--- 2021/rust/src/day3/mod.rs
+++ 2021/rust/src/day3/mod.rs
@@ -2,13 +2,22 @@ use super::common;
 
 pub fn run() {
     println!("Part 1 result: {}", part1());
+    println!("Part 2 result: {}", part2());
 }
 
 fn part1() -> usize {
-    let input = parse_input(common::read_file("src/day3/problem-input.txt"));
-    let rates = calc_rates(&input);
+    evaluate_rates(calc_rates)
+}
 
-    rates.epsilon as usize * rates.gamma as usize
+fn part2() -> usize {
+    evaluate_rates(calc_collapsed_rates)
+}
+
+fn evaluate_rates(f: fn(&Vec<String>) -> Rates) -> usize {
+    let input = parse_input(common::read_file("src/day3/problem-input.txt"));
+    let rates = f(&input);
+
+    rates.rate1 as usize * rates.rate0 as usize
 }
 
 #[allow(dead_code)]
@@ -33,24 +42,31 @@ fn binary_to_dec(bin: &str) -> u32 {
 
 #[derive(Eq, PartialEq, Debug)]
 struct Rates {
-    pub gamma: u32,
-    pub epsilon: u32
+    pub rate0: u32,
+    pub rate1: u32
 }
 
 impl Rates {
-    pub fn new(gamma: u32, epsilon: u32) -> Self {
-        Self { gamma, epsilon }
+    pub fn new(rate0: u32, rate1: u32) -> Self {
+        Self { rate0, rate1 }
     }
 }
 
-fn calc_rates(input: &Vec<String>) -> Rates {
-    let threshold = input.len() as u32 / 2;
-    let mut freq = [0u32; 12];
+fn calc_freq(input: &Vec<String>) -> Vec<u32> {
+    let bin_width = input.iter().next().expect("No data").len();
+    let mut freq = vec![0u32; bin_width];
 
     input.iter().for_each(
         |b| b.chars().enumerate().for_each(|(i, c)| if c == '1' { freq[i] += 1 }));
 
+    freq
+}
+
+fn calc_rates(input: &Vec<String>) -> Rates {
+    let freq = calc_freq(input);
+
     // Problem statement relies on all freq being above or below threshold
+    let threshold = input.len() as u32 / 2;
     assert!(freq.iter().all(|f| *f != threshold));
 
     let gamma_bin = freq.iter()
@@ -64,6 +80,36 @@ fn calc_rates(input: &Vec<String>) -> Rates {
     Rates::new(binary_to_dec(&gamma_bin), binary_to_dec(&epsilon_bin))
 }
 
+fn calc_collapsed_rates(input: &Vec<String>) -> Rates {
+    let o2 = calc_collapsed_rate(input, |freq0, freq1| if freq1 >= freq0 { '1' } else { '0' });
+    let co2 = calc_collapsed_rate(input, |freq0, freq1| if freq1 < freq0 { '1' } else { '0' });
+
+    Rates::new(o2, co2)
+}
+
+fn calc_collapsed_rate(input: &Vec<String>, bin_criteria: fn(u32, u32) -> char) -> u32 {
+    let mut eligible = input.clone();
+    let bin_width = input.iter().next().expect("No data").len();
+    let mut ix = 0usize;
+
+    while eligible.len() != 1 {
+        assert!(ix < bin_width);    // Exact solution exists within the input set, per problem statement
+
+        let freq = calc_freq(&eligible);
+        let criteria = bin_criteria(eligible.len() as u32 - freq[ix], freq[ix]);
+
+        eligible = eligible.iter()
+            .filter(|&b| b.chars().nth(ix).expect("Cannot index string") == criteria)
+            .cloned()
+            .collect();
+
+        ix += 1;
+    }
+
+    assert_eq!(eligible.len(), 1);
+    binary_to_dec(eligible.iter().next().expect("Invalid final result"))
+ }
+
 fn parse_input(input: String) -> Vec<String> {
     input.lines().map(&str::to_string).collect()
 }
@@ -72,16 +118,28 @@ fn parse_input(input: String) -> Vec<String> {
 #[cfg(test)]
 mod test {
     use super::common;
-    use crate::day3::{calc_rates, parse_input, part1, Rates};
+    use crate::day3::{calc_rates, calc_collapsed_rates, parse_input, part1, part2, Rates};
 
     #[test]
     fn test_rate_calc() {
         let input = parse_input(common::read_file("src/day3/test-input.txt"));
-        assert_eq!(calc_rates(&input), Rates::new(22, 4073));
+        assert_eq!(calc_rates(&input), Rates::new(22, 9));
+    }
+
+    #[test]
+    fn test_collapsed_rate_calc() {
+        let input = parse_input(common::read_file("src/day3/test-input.txt"));
+        assert_eq!(calc_collapsed_rates(&input), Rates::new(23, 10));
+
     }
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), 12);
+        assert_eq!(part1(), 3912944);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(), 4996233);
     }
 }
