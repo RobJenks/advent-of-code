560021bf99fdb8233116ed79b8c8dc4a935655ed
Rob Jenks <robjenks@gmail.com>
2022-10-17 18:11:48 -0400

    Add support for multi generation iteration

--- 2021/rust/src/day6/mod.rs
+++ 2021/rust/src/day6/mod.rs
@@ -27,6 +27,9 @@ fn iterate(values: &mut Vec<u32>) {
     }
 }
 
+fn iterate_n(values: &mut Vec<u32>, num_generations: u32) {
+    (0..num_generations).into_iter().for_each(|_| iterate(values));
+}
 
 fn parse_input(input: String) -> Vec<u32> {
     input.split(",")
@@ -38,7 +41,7 @@ fn parse_input(input: String) -> Vec<u32> {
 
 #[cfg(test)]
 mod test {
-    use crate::day6::{part1, part2, iterate};
+    use crate::day6::{part1, part2, iterate, iterate_n};
 
     #[test]
     fn test_iteration() {
@@ -52,6 +55,22 @@ mod test {
         assert_eq!(values, vec![0,1,0,5,6,7,8]);
         iterate(&mut values);
         assert_eq!(values, vec![6,0,6,4,5,6,7,8,8]);
+    }
+
+    #[test]
+    fn test_multi_generation() {
+        let mut values = vec![3,4,3,1,2];
+
+        iterate_n(&mut values, 18);
+        assert_eq!(values.len(), 26);
+    }
+
+    #[test]
+    fn test_multi_generation_longer() {
+        let mut values = vec![3,4,3,1,2];
+
+        iterate_n(&mut values, 80);
+        assert_eq!(values.len(), 5934);
     }
 
     #[test]
