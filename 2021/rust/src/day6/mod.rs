2e67fa17b1756089447c5de51223ac59a9f701b9
Rob Jenks <robjenks@gmail.com>
2022-10-17 18:55:06 -0400

    Add day 6, part 2 solution

--- 2021/rust/src/day6/mod.rs
+++ 2021/rust/src/day6/mod.rs
@@ -1,4 +1,5 @@
 use crate::common;
+use std::collections::VecDeque;
 
 pub fn run() {
     println!("Part 1 result: {}", part1());
@@ -7,16 +8,30 @@ pub fn run() {
 
 fn part1() -> usize {
     let mut values = parse_input(common::read_file("src/day6/problem-input.txt"));
-    iterate_n(&mut values, 80);
+    iterate_direct_n(&mut values, 80);
 
     values.len()
 }
 
 fn part2() -> usize {
-    12
+    let values = parse_input(common::read_file("src/day6/problem-input.txt"));
+    iterate_fast(&values, 256)
 }
 
-fn iterate(values: &mut Vec<u32>) {
+fn iterate_fast(values: &Vec<u32>, iterations: u32) -> usize {
+    let mut days_remaining = [0usize; 9].into_iter().collect::<VecDeque<usize>>();
+
+    values.iter().for_each(|&x| days_remaining[x as usize] += 1);
+
+    (0..iterations).into_iter().for_each(|_| {
+        days_remaining.rotate_left(1);
+        days_remaining[6] += days_remaining[8];
+    });
+
+    days_remaining.iter().sum()
+}
+
+fn iterate_direct(values: &mut Vec<u32>) {
     let n = values.len();
     for i in 0..n {
         let val = values[i];
@@ -30,8 +45,8 @@ fn iterate(values: &mut Vec<u32>) {
     }
 }
 
-fn iterate_n(values: &mut Vec<u32>, num_generations: u32) {
-    (0..num_generations).into_iter().for_each(|_| iterate(values));
+fn iterate_direct_n(values: &mut Vec<u32>, num_generations: u32) {
+    (0..num_generations).into_iter().for_each(|_| iterate_direct(values));
 }
 
 fn parse_input(input: String) -> Vec<u32> {
@@ -44,19 +59,19 @@ fn parse_input(input: String) -> Vec<u32> {
 
 #[cfg(test)]
 mod test {
-    use crate::day6::{part1, part2, iterate, iterate_n};
+    use crate::day6::{part1, part2, iterate_direct, iterate_direct_n, iterate_fast};
 
     #[test]
     fn test_iteration() {
         let mut values = vec![3,4,3,1,2];
 
-        iterate(&mut values);
+        iterate_direct(&mut values);
         assert_eq!(values, vec![2,3,2,0,1]);
-        iterate(&mut values);
+        iterate_direct(&mut values);
         assert_eq!(values, vec![1,2,1,6,0,8]);
-        iterate(&mut values);
+        iterate_direct(&mut values);
         assert_eq!(values, vec![0,1,0,5,6,7,8]);
-        iterate(&mut values);
+        iterate_direct(&mut values);
         assert_eq!(values, vec![6,0,6,4,5,6,7,8,8]);
     }
 
@@ -64,7 +79,7 @@ mod test {
     fn test_multi_generation() {
         let mut values = vec![3,4,3,1,2];
 
-        iterate_n(&mut values, 18);
+        iterate_direct_n(&mut values, 18);
         assert_eq!(values.len(), 26);
     }
 
@@ -72,8 +87,23 @@ mod test {
     fn test_multi_generation_longer() {
         let mut values = vec![3,4,3,1,2];
 
-        iterate_n(&mut values, 80);
+        iterate_direct_n(&mut values, 80);
         assert_eq!(values.len(), 5934);
+    }
+
+    #[test]
+    fn test_fast_iteration() {
+        let values = vec![3,4,3,1,2];
+
+        assert_eq!(iterate_fast(&values, 18), 26);
+        assert_eq!(iterate_fast(&values, 80), 5934);
+    }
+
+    #[test]
+    fn test_fast_iteration_exponential() {
+        let values = vec![3,4,3,1,2];
+
+        assert_eq!(iterate_fast(&values, 256), 26984457539);
     }
 
     #[test]
@@ -83,7 +113,7 @@ mod test {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), 12);
+        assert_eq!(part2(), 1695929023803);
     }
 }
 
